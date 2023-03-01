ba390407203525e8e86560f1250c081e67f7e4f4
DLJ949 <djohnson949@yahoo.com>
2023-02-28 17:31:16 -0800

    Moved data plus started app.R

--- Shopping-n-SocialMedia/app.R
+++ Shopping-n-SocialMedia/app.R
@@ -1,13 +1,13 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+## INFO 201 FINAL PROJECT
+# Names:
+# Jordan Lee
+# Kyra Diaz
+# Zareen Tasnim
+# Dylan Johnson
+
 
 library(shiny)
+library(tidyverse)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
